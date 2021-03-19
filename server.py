a1f96d74eaf5e591aae7482851964c82727e7d28
Fabian Vieri <vierifabian@gmail.com>
2021-03-19 21:03:35 +0700

    init

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, Response 
+import cv2
+import numpy as np
+import jsonpickle
+
+app = Flask(__name__) 
+
+@app.route('/')  
+def hello_world(): 
+    return '<h1>Hello from server</h1>'
+
+@app.route('/image', methods=["POST"])  
+def image(): 
+    r = request
+    nparr = np.fromstring(r.data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+  
+if __name__ == '__main__': 
+    app.run() 
