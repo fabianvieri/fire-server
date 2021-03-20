22cbaef2b235fa9dac3d88c8f0819c728ad1144d
Fabian Vieri <vierifabian@gmail.com>
2021-03-20 10:41:57 +0700

    add count

--- app.py
+++ app.py
@@ -4,19 +4,29 @@ import numpy as np
 import jsonpickle
 import os
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+count = 0
 
 @app.route('/')  
-def hello_world(): 
+def root(): 
     return '<h1>Hello from server</h1>'
 
+@app.route('/count')  
+def root():
+    global count
+    count = count + 1 
+    return Response(response=count, status=200)
+
 @app.route('/image', methods=["POST"])  
-def image(): 
+def image():
+    global count 
     r = request
     nparr = np.fromstring(r.data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
     response_pickled = jsonpickle.encode(response)
+    count = count + 1
     return Response(response=response_pickled, status=200, mimetype="application/json")
   
 if __name__ == '__main__': 
